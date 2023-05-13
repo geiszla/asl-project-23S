fcf6d090cc3234bcbbabb99cbcc38f7a7780b05b
Alec Pauli <paulia@student.ethz.ch>
2023-05-13 21:44:20 +0200

    Update src/basefunctions.cpp

--- src/basefunctions.cpp
+++ src/basefunctions.cpp
@@ -4,11 +4,6 @@
 #include "time.h"
 #include <stdlib.h>
 const double trennung = 0.000000000000000001;// 10^-18
-
-#define dbl_prec 53
-#define binSize 45
-
-
 // compile with g++ -std=c++11 ./main.cpp
 // error free transforms 
 /*
@@ -148,12 +143,13 @@ void addition(double *a, double *b, double *s, int length_a,int length_b, int le
  * Input a and b of length k 
  * Output r of length k
 */
-void multiplication(double *a, double *b, double *r){
-    int length = sizeof(a)/sizeof(a[0]);int k = length;
-    double* err = (double *)malloc((k*k+k-1)*sizeof(double));
-    double* pi_res = (double *)malloc(length*sizeof(double));
-    twoMultFMA(a[0],b[0],&(pi_res[0]), &(err[0]));
-    for(int n=1; n<length; n++){
+void multiplication(double *a, double *b, double *r,int sizea, int sizeb, int sizer){
+    int k = sizea;
+    double* err = (double *)malloc((sizea*sizea -1)*sizeof(double));
+    double* pi_res = (double *)malloc(sizea*sizeof(double));
+    double* r_ext = (double *)malloc(sizea*sizeof(double));
+    twoMultFMA(a[0],b[0],&(r_ext[0]), &(err[0]));
+    for(int n=1; n<sizea; n++){
         double*  e_tmp = new double[n];
         double*  p = new double[n];
         for(int i = 0; i<=n; i++){
@@ -167,139 +163,43 @@ void multiplication(double *a, double *b, double *r){
         for(int b =n+1; b<n*n+n; b++){
             tmp2[b]=err[b-n];
         }
+        // write result into tmp
         vecSum(tmp2, tmp,1);
-        r[n] = tmp[0];
-        for(int b = 1; b<=n*n+n; b++){
-            err[b-1] = tmp[b];
+        // r_N = tmp[0]
+        r_ext[n] = 0;
+        // now write e 0:n^2 -1
+        for(int b =0; b< n*n-1; b++){
+            err[b]= tmp[b+1];
         }
-        for(int b = 0; b<=n; b++){
-            err[n*n+n+ b+1] = e_tmp[b];
+
+        // now compute e[0:(n+1)^2 -1] <- e[0:n^2 + n -1],e[0:n]
+        int count = 0;
+        for(int b =0; b<=n^2 +n-1; b++){
+            err[count] = err[b]; count++;
         }
+        for(int b =0; b<=n; b++){
+            err[count] = err[b]; count++;
+        }
+        // here compute line 9-11
+        for ( int i =1; i<k; i++){
+            r_ext[k] = r_ext[k] + a[i]*b[k-i];
+        }
+        // compute line 12 -14
+         for ( int i =0; i<k*k; i++){
+            r_ext[k] = r_ext[k] + err[i];
+        }
+
+
         
         delete[] e_tmp;delete[] p; delete[] tmp;delete[] tmp2;
     }
 
-    for(int i = 1; i<=k-1; i++){
-        r[k]= r[k]+ err[i];
+    for(int i = 1; i<=sizea*sizea-1; i++){
+        r_ext[k]= r_ext[k]+ err[i];
     }
-    renormalizationalgorithm(r,2,r,k);
+    renormalizationalgorithm(r_ext,k+1,r,sizea);
     // return
 }
 
-// helper
-int exponent(double d)
-{
-  int result;
-  frexp(d,&result);
-  return result;
-}
-
-void deposit(double *p, double *b1, double *b2) {
-	twoSum(*b1, *p, b1, p);
-	*b2 = *b2 + *p;
-}
-/**
-Implementation of Accumulate algorithm (Algorithm 2, Paper 2)
-
-Input: 	double p, e
-		vector<double> b
-		int sh, l
-Output: vector<double> b
-
-**/
-
-void accumulate(double p, double e, double *b, int sh, int l) {
-	int c = dbl_prec - binSize - 1;
-	if (l < binSize - 2*c - 1){
-		deposit(&p, &b[sh], &b[sh+1]);
-		deposit(&e, &b[sh+1], &b[sh+2]);
-	} else if (l < binSize - c){
-		deposit(&p, &b[sh], &b[sh+1]);
-		twoSum(b[sh+1], e, &b[sh+1], &e);
-		deposit(&e, &b[sh+2], &b[sh+3]);
-	} else {
-		twoSum(b[sh], p, &b[sh], &p);
-		deposit(&p, &b[sh+1], &b[sh+2]);
-		deposit(&e, &b[sh+2], &b[sh+3]);
-	}
-}
-
-/**
-Implementation of Renormalize algorithm (Algorithm 3, Paper 2)
-
-Input: 	vector<double> x (size n)
-		int n, k
-Output: vector<double> r (ulp-nonoverlapping) (size k)
-**/
-
-void renormalize(double *x,double* r, int n, int k) {
-	double eps = x[0];
-	
-	int j = 0;
-	int i = 1;
-	while (i < n && j < k) {
-		twoSum(eps, x[i], &r[j], &eps);
-		if (eps == 0) { // no overflow
-			eps = r[j];
-		} else {
-			j++;
-		}
-		i++;
-	}
-	if (eps != 0 && j < k) {
-		r[j] = eps;
-	}
-}
-
-
-/**
-Implementation of Multiplication algorithm (Algorithm 1, Paper 2)
-
-Input: x vector size n (ulp-nonoverlapping)
-		y vector size m (ulp-nonoverlapping)
-Output: pi vector size r (ulp-nonoverlapping) = x*y
-
-Constraint: n >=m
-**/
-
-double* mult2(double* x, double* y,double*pi, int n, int m, int r){
-	int const LBN = r*dbl_prec/binSize; // number of allocated bins 
-	double* B = (double *)malloc((LBN+2)*sizeof(double));
-	// get sum of first exponents
-	int e = exponent(x[0]) + exponent(y[0]);
-	
-	// initialize each Bin with starting value
-	for (int i=0; i<LBN+2;i++){
-		B[i] = ldexp(1.5,e-(i+1)*binSize+dbl_prec-1); // 1.5*2^(e-(i+1)b+p-1)
-	}
-	int j,l,sh;
-	double p, err;
-	for(int i=0; i<= fmin(n-1, r); i++){
-		for(j=0;j<= fmin(m-1,r-1-i); j++){
-			//p = two_prod(x[i], y[j], &err); this leads to more similar result as CAMPARY
-			twoMultFMA(x[i], y[j],&p,&err);
-			l = e- exponent(x[i]) - exponent(y[j]); 
-			sh = floor(l/binSize); // bin of the first pair
-			l = l- sh*binSize; // number of leading bits
-			accumulate(p,err,B,sh,l); // add to correct bins
-		}
-		j-=1;
-		if (j < m-1){ //  I don't get what this part does
-			p = x[i]*y[j];
-			l = e- exponent(x[i]) - exponent(y[j]); 
-			sh = floor(l/binSize); 
-			l = l- sh*binSize; 
-			accumulate(p,0.,B,sh,l);
-		}
-	}
-	for (int i=0;  i<LBN+2;i++){
-		B[i] = B[i]-ldexp(1.5,e-(i+1)*binSize+dbl_prec-1); // B_i - 1.5*2^(e-(i+1)b+p-1)
-	}
-
-	
-	//renorm_rand2L(B, pi, LBN+2,r);
-	//fast_VecSumErrBranch(B,pi,LBN+2,r);
-	renormalize(B,pi,LBN+2,r);
-	return pi;
-}
+// testing stuff
 
