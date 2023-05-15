4ac8a6c3dfab47f289babc3568e471a68245b714
Andras Geiszl <ageiszl@student.ethz.ch>
2023-05-15 21:29:08 +0200

    Document implementation evaluation function

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -4,50 +4,59 @@
 #include "measure.cpp"
 #include "testalgo5and6.cpp"
 
-enum FPFunction
+enum Algorithm
 {
-    two_sum_function,
-    two_mult_FMA_function,
-    vec_sum_function,
-    vec_sum_err_branch_function,
-    vec_sum_err_function,
-    renormalization_function,
-    addition_function,
-    multiplication_function,
-    multiplication2_function
+    two_sum_algorithm,
+    two_mult_FMA_algorithm,
+    vec_sum_algorithm,
+    vec_sum_err_branch_algorithm,
+    vec_sum_err_algorithm,
+    renormalization_algorithm,
+    addition_algorithm,
+    multiplication_algorithm,
+    multiplication2_algorithm
 };
 
 template <typename T_compute_function>
-map<FPFunction, T_compute_function> tests{
-    {two_sum_function, (T_compute_function)testtwosum},
-    {two_mult_FMA_function, (T_compute_function)testfastfma},
-    {renormalization_function, (T_compute_function)testrenormalization},
-    {addition_function, (T_compute_function)testaddition}};
+map<Algorithm, T_compute_function> tests{
+    {two_sum_algorithm, (T_compute_function)testtwosum},
+    {two_mult_FMA_algorithm, (T_compute_function)testfastfma},
+    {renormalization_algorithm, (T_compute_function)testrenormalization},
+    {addition_algorithm, (T_compute_function)testaddition}};
 
 template <typename T_measure_function>
-map<FPFunction, T_measure_function> measurement_functions{
-    {two_sum_function, (T_measure_function)measure_two_sum},
-    {two_mult_FMA_function, (T_measure_function)measure_two_mult_FMA},
-    {vec_sum_function, (T_measure_function)measure_vec_sum},
-    {vec_sum_err_branch_function, (T_measure_function)measure_vec_sum_err_branch},
-    {vec_sum_err_function, (T_measure_function)measure_vec_sum_err},
-    {renormalization_function, (T_measure_function)measure_renormalization},
-    {addition_function, (T_measure_function)measure_addition},
-    {multiplication_function, (T_measure_function)measure_multiplication},
-    {multiplication2_function, (T_measure_function)measure_multiplication2}};
+map<Algorithm, T_measure_function> measurement_functions{
+    {two_sum_algorithm, (T_measure_function)measure_two_sum},
+    {two_mult_FMA_algorithm, (T_measure_function)measure_two_mult_FMA},
+    {vec_sum_algorithm, (T_measure_function)measure_vec_sum},
+    {vec_sum_err_branch_algorithm, (T_measure_function)measure_vec_sum_err_branch},
+    {vec_sum_err_algorithm, (T_measure_function)measure_vec_sum_err},
+    {renormalization_algorithm, (T_measure_function)measure_renormalization},
+    {addition_algorithm, (T_measure_function)measure_addition},
+    {multiplication_algorithm, (T_measure_function)measure_multiplication},
+    {multiplication2_algorithm, (T_measure_function)measure_multiplication2}};
 
+/**
+ * Evaluates an implementation of an algorithm
+ *
+ * @param implementation The function implementation to evaluate
+ * @param name Name of the implementation (only used for display in command line)
+ * @param algorithm The algorithm from `Algorithm` enum that is being implemented
+ * @param naive_runtime If passed, the speedup of the implementation compared to this
+ * will be calculated
+ */
 template <typename T_compute_return, class... T_compute_arguments>
 void evaluate_implementation(
     T_compute_return (*implementation)(T_compute_arguments...),
     string name,
-    FPFunction function_type,
+    Algorithm algorithm,
     double naive_runtime = 0.)
 {
     auto typed_tests = tests<void (*)(T_compute_return (*)(T_compute_arguments...))>;
 
-    if (typed_tests.count(function_type) > 0)
+    if (typed_tests.count(algorithm) > 0)
     {
-        typed_tests[function_type](implementation);
+        typed_tests[algorithm](implementation);
     }
     else
     {
@@ -55,7 +64,7 @@ void evaluate_implementation(
     }
 
     auto measurement_function = measurement_functions<
-        double (*)(T_compute_return (*)(T_compute_arguments...), int)>[function_type];
+        double (*)(T_compute_return (*)(T_compute_arguments...), int)>[algorithm];
 
     double runtime = measurement_function(implementation, DEFAULT_TERM_COUNT);
 
--- src/playground.cpp
+++ src/playground.cpp
@@ -7,5 +7,5 @@ int main()
   // Pass the naive implementation's runtime to `evaluate_implementation` to calculate speedup
   double naive_runtime = measure_vec_sum();
 
-  evaluate_implementation(vecSum, "vec_sum_slow", vec_sum_function, naive_runtime);
+  evaluate_implementation(vecSum, "vec_sum_slow", vec_sum_algorithm, naive_runtime);
 }
