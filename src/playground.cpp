63cd2f1f5e47cf6374db66dfb45bb6cd92582004
Andras Geiszl <ageiszl@student.ethz.ch>
2023-05-15 21:19:09 +0200

    Fix comment in playground

--- src/playground.cpp
+++ src/playground.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-  // Pass the naive implementation's runtime to `test_implementation` to calculate speedup
+  // Pass the naive implementation's runtime to `evaluate_implementation` to calculate speedup
   double naive_runtime = measure_vec_sum();
 
   evaluate_implementation(vecSum, "vec_sum_slow", vec_sum_function, naive_runtime);
