2565fe094ea59e7da088195c0dd66d5ec339f36c
Andras Geiszl <ageiszl@student.ethz.ch>
2023-05-19 21:16:09 +0200

    Start optimizing vecSum

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,5 +1,10 @@
 #include "evaluate.cpp"
 
+extern "C"
+{
+  #include "vec_sum_optimizations.c"
+}
+
 // Compile with `g++ -std=c++17 ./benchmark.cpp` (plus additional optimization flags)
 
 int main()
@@ -7,5 +12,6 @@ int main()
   // Pass the naive implementation's runtime to `evaluate_implementation` to calculate speedup
   double naive_runtime = measure_vec_sum();
 
-  evaluate_implementation(vecSum, "vec_sum_slow", vec_sum_algorithm, naive_runtime);
+  // evaluate_implementation(renormalizationalgorithm, "renormalization", renormalization_algorithm);
+  evaluate_implementation(vecSum3, "vecSum3", vec_sum_algorithm, naive_runtime);
 }
---
+++ src/vec_sum_optimizations.c
@@ -0,0 +1,72 @@
+void vecSum2(double *x, double *e_res, int in_out_size)
+{
+  double *s = (double *)alloca(in_out_size * sizeof(double));
+
+  s[in_out_size - 1] = x[in_out_size - 1];
+
+  for (int i = in_out_size - 2; i >= 0; i--)
+  {
+    double a = x[i];
+    double b = s[i + 1];
+
+    double s2 = a + b;
+    double t = s2 - b;
+
+    double e = (a - t) + (b - (s2 - t));
+
+    s[i] = s2;
+    e_res[i] = e;
+  }
+
+  e_res[0] = s[0];
+
+  return;
+}
+
+void vecSum3(double *x, double *e_res, int in_out_size)
+{
+  double s_last = x[in_out_size - 1];
+
+  for (int i = in_out_size - 2; i >= 0; i--)
+  {
+    double a = x[i];
+    double b = s_last;
+
+    double s2 = a + b;
+    double t = s2 - b;
+
+    double e = (a - t) + (b - (s2 - t));
+
+    s_last = s2;
+    e_res[i] = e;
+  }
+
+  e_res[0] = s_last;
+
+  return;
+}
+
+void renormalizationalgorithm2(double x[], int size_of_x, double f[], int m)
+{
+  int length = size_of_x;
+
+  double *e = (double *)alloca(length * sizeof(double));
+  double *temp = (double *)alloca(length * sizeof(double));
+  double *g = (double *)alloca(m * sizeof(double));
+
+  vecSum(x, e, size_of_x);
+  vecSumErrBranch(e, length, m + 1, temp);
+
+  for (int i = 0; i <= m - 2; i++)
+  {
+    vecSumErr(&temp[i], m, g);
+    for (int b = 0; b < m; b++)
+    {
+      double tmp = g[b];
+      temp[b + i] = tmp;
+    }
+    double t = temp[i];
+    f[i] = t;
+  }
+  f[m - 1] = temp[m - 1];
+}
