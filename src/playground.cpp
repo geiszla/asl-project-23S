d56d9cbbdc10fcec799a04c97bc101dad7fcdd02
Andras Geiszl <ageiszl@student.ethz.ch>
2023-05-21 19:35:27 +0200

    Optimize vecSumErrBranch

--- src/playground.cpp
+++ src/playground.cpp
@@ -2,8 +2,9 @@
 
 extern "C"
 {
-  // #include "vec_sum_optimizations.c"
-  #include "vec_sum_err_optimizations.c"
+// #include "vec_sum_optimizations.c"
+// #include "vec_sum_err_optimizations.c"
+  #include "vec_sum_err_branch_optimizations.c"
 }
 
 // Compile with `g++ -std=c++17 ./benchmark.cpp` (plus additional optimization flags)
@@ -18,8 +19,15 @@ int main()
   // evaluate_implementation(vecSum3, "vecSum3", vec_sum_algorithm, naive_runtime);
 
   /* vecSumErr */
-  double naive_runtime = measure_vec_sum_err();
+  // double naive_runtime = measure_vec_sum_err();
 
-  evaluate_implementation(renormalizationalgorithm3, "renormalization3", renormalization_algorithm);
-  evaluate_implementation(vecSumErr2, "vecSumErr2", vec_sum_err_algorithm, naive_runtime);
+  // evaluate_implementation(renormalizationalgorithm3, "renormalization3", renormalization_algorithm);
+  // evaluate_implementation(vecSumErr2, "vecSumErr2", vec_sum_err_algorithm, naive_runtime);
+
+  /* vecSumErrBranch */
+  double naive_runtime = measure_vec_sum_err_branch();
+
+  evaluate_implementation(renormalizationalgorithm4, "renormalization4", renormalization_algorithm);
+  evaluate_implementation(vecSumErrBranch2, "vecSumErrBranch2", vec_sum_err_branch_algorithm,
+                          naive_runtime);
 }
---
+++ src/vec_sum_err_branch_optimizations.c
@@ -0,0 +1,62 @@
+void vecSumErrBranch2(double *e, int n, int m, double *f)
+{
+  double err = e[0];
+
+  int l = m - 1;
+  int j = 0;
+
+  for (int i = 1; i <= n - 1; i++)
+  {
+    double b = e[i];
+
+    double s = err + b;
+    double t = s - b;
+
+    err = (err - t) + (b - (s - t));
+
+    f[j] = s;
+
+    if (err == 0)
+    {
+      err = s;
+    }
+    else if (j >= l)
+    {
+      return;
+    }
+    else
+    {
+      j++;
+    }
+  }
+
+  if (err != 0 && j < m)
+  {
+    f[j] = err;
+  }
+}
+
+void renormalizationalgorithm4(double x[], int size_of_x, double f[], int m)
+{
+  int length = size_of_x;
+
+  double *e = (double *)alloca(length * sizeof(double));
+  double *temp = (double *)alloca(length * sizeof(double));
+  double *g = (double *)alloca(m * sizeof(double));
+
+  vecSum(x, e, size_of_x);
+  vecSumErrBranch2(e, length, m + 1, temp);
+
+  for (int i = 0; i <= m - 2; i++)
+  {
+    vecSumErr(&temp[i], m, g);
+    for (int b = 0; b < m; b++)
+    {
+      double tmp = g[b];
+      temp[b + i] = tmp;
+    }
+    double t = temp[i];
+    f[i] = t;
+  }
+  f[m - 1] = temp[m - 1];
+}
